ca6691bb6421861e58215280192fe33e2c17e8f2
Garrett Huon <ghuon407@gmail.com>
2021-10-08 10:26:33 -0400

    added model

---
+++ Hackathon1/Hackathon1/Models/VideoModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hackathon1.Models
+{
+    public class VideoModel
+    {
+        public VideoModel()
+        { }
+
+        public VideoModel(VideoModel video)
+        {
+            this.Id = video.Id;
+            this.Video = video.Video;
+        }
+
+        public string Video { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}, Video: {this.Video}";
+        }
+        public bool Equals(VideoModel video)
+        {
+            return this.Video == video.Video;
+        }
+    }
+}
