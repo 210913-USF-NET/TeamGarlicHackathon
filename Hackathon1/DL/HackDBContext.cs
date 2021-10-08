28ddd5ce65c85521afdbdd20609d60799c794ef7
Garrett Huon <ghuon407@gmail.com>
2021-10-08 14:37:26 -0400

    Migration success

--- Hackathon1/DL/HackDBContext.cs
+++ Hackathon1/DL/HackDBContext.cs
@@ -4,12 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Models;
 
 namespace DL
 {
     public class HackDBContext : DbContext
     {
-        public HackDBContext() : base() {}
+        public HackDBContext() : base() { }
         public HackDBContext(DbContextOptions options) : base(options) { }
+        public DbSet<ModelVideo> Vids { get; set; }
+
+        
     }
 }
---
+++ Hackathon1/Models/ModelVideo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class ModelVideo
+    {
+        public ModelVideo()
+        { }
+
+        public ModelVideo(ModelVideo video)
+        {
+            this.Id = video.Id;
+            this.Vid = video.Vid;
+        }
+
+        public string Vid { get; set; }
+        public int Id { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {this.Id}, Video: {this.Vid}";
+        }
+        public bool Equals(ModelVideo video)
+        {
+            return this.Vid == video.Vid;
+        }
+
+        //test
+    }
+}
